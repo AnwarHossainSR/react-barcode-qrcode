e77ad47bb6f068c28e548e5472397d0ea06cb63b
Anwar Hossain <anwar.sarker@bjitgroup.com>
2022-06-26 19:29:27 +0600

    demo of barcode scanner

--- src/components/BarCodeScanner.jsx
+++ src/components/BarCodeScanner.jsx
@@ -1,5 +1,31 @@
-const BarCodeScanner = () => {
-  return <div></div>;
-};
+import { Component } from "react";
+import BarcodeReader from "react-barcode-reader";
+
+class BarCodeScanner extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: "No result",
+    };
+
+    this.handleScan = this.handleScan.bind(this);
+  }
+  handleScan(data) {
+    this.setState({
+      result: data,
+    });
+  }
+  handleError(err) {
+    console.error(err);
+  }
+  render() {
+    return (
+      <div>
+        <BarcodeReader onError={this.handleError} onScan={this.handleScan} />
+        <p>{this.state.result}</p>
+      </div>
+    );
+  }
+}
 
 export default BarCodeScanner;
