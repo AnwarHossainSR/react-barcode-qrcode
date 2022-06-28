5adb99206b80fee4a35dae5219fd64674e670c38
Restyled.io <commits@restyled.io>
2022-06-28 03:37:42 +0000

    Restyled by clang-format

--- src/components/Scanner.js
+++ src/components/Scanner.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
-import config from "./config.json";
 import Quagga from "quagga";
+import React, {useEffect} from "react";
+
+import config from "./config.json";
 
 const Scanner = props => {
-  const { onDetected } = props;
+  const {onDetected} = props;
 
   useEffect(() => {
     Quagga.init(config, err => {
@@ -11,50 +12,35 @@ const Scanner = props => {
         console.log(err, "error msg");
       }
       Quagga.start();
-      return () => {
-        Quagga.stop()
-      }
+      return () => { Quagga.stop() }
     });
 
-    //detecting boxes on stream
+    // detecting boxes on stream
     Quagga.onProcessed(result => {
       var drawingCtx = Quagga.canvas.ctx.overlay,
-        drawingCanvas = Quagga.canvas.dom.overlay;
+          drawingCanvas = Quagga.canvas.dom.overlay;
 
       if (result) {
         if (result.boxes) {
-          drawingCtx.clearRect(
-            0,
-            0,
-            Number(drawingCanvas.getAttribute("width")),
-            Number(drawingCanvas.getAttribute("height"))
-          );
-          result.boxes
-            .filter(function(box) {
-              return box !== result.box;
-            })
-            .forEach(function(box) {
-              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
-                color: "green",
-                lineWidth: 2
+          drawingCtx.clearRect(0, 0,
+                               Number(drawingCanvas.getAttribute("width")),
+                               Number(drawingCanvas.getAttribute("height")));
+          result.boxes.filter(function(box) { return box !== result.box; })
+              .forEach(function(box) {
+                Quagga.ImageDebug.drawPath(box, {x : 0, y : 1}, drawingCtx,
+                                           {color : "green", lineWidth : 2});
               });
-            });
         }
 
         if (result.box) {
-          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
-            color: "#00F",
-            lineWidth: 2
-          });
+          Quagga.ImageDebug.drawPath(result.box, {x : 0, y : 1}, drawingCtx,
+                                     {color : "#00F", lineWidth : 2});
         }
 
         if (result.codeResult && result.codeResult.code) {
-          Quagga.ImageDebug.drawPath(
-            result.line,
-            { x: "x", y: "y" },
-            drawingCtx,
-            { color: "red", lineWidth: 3 }
-          );
+          Quagga.ImageDebug.drawPath(result.line, {x : "x", y : "y"},
+                                     drawingCtx,
+                                     {color : "red", lineWidth : 3});
         }
       }
     });
@@ -62,16 +48,13 @@ const Scanner = props => {
     Quagga.onDetected(detected);
   }, []);
 
-  const detected = result => {
-    onDetected(result.codeResult.code);
-  };
+  const detected = result => { onDetected(result.codeResult.code); };
 
   return (
-    // If you do not specify a target,
-    // QuaggaJS would look for an element that matches
-    // the CSS selector #interactive.viewport
-    <div id="interactive" className="viewport" />
-  );
+      // If you do not specify a target,
+      // QuaggaJS would look for an element that matches
+      // the CSS selector #interactive.viewport
+      <div id = "interactive" className = "viewport" />);
 };
 
 export default Scanner;
